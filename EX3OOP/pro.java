681de6246e42be416cf2ca285a46edb06494a64d
naseerabacker <95747989+naseerabacker@users.noreply.github.com>
2022-04-29 14:35:14 +0530

    Add files via upload

--- EX3OOP/pro.java
+++ EX3OOP/pro.java
@@ -28,21 +28,21 @@ int price;
             pro obj3=new pro();
       
 
-            System.out.print("Enter person 1 "+" pcode,pname,price: \n ");
+            System.out.print("Enter product1 "+" pcode,pname,price: \n ");
             a1=s.nextInt();
             c1=s.next();
             b1=s.nextInt();
             obj1.setdata(a1,b1,c1);//invoking setdata
 
            
-            System.out.print("Enter person 2 "+" pcode,pname,price:\n ");
+            System.out.print("Enter product 2 "+" pcode,pname,price:\n ");
             a2=s.nextInt();
             c2=s.next();
             b2=s.nextInt();
             obj2.setdata(a2,b2,c2);
 
           
-            System.out.print("Enter person 3 "+" pcode,pname,price:\n ");
+            System.out.print("Enter product 3 "+" pcode,pname,price:\n ");
             a3=s.nextInt();
             c3=s.next();
             b3=s.nextInt();
@@ -54,7 +54,7 @@ int price;
             System.out.print("\n");
 //comparing and finding smallif(obj1.price<obj2.price && obj1.price<obj3.price)  
            if(obj1.price<obj2.price && obj1.price<obj3.price)  
-             System.out.println(" Product code= " +obj1.pcode+" , Product        name="+obj1.pname+" , price="+obj1.price); 
+             System.out.println(" Product code= " +obj1.pcode+" , Productname="+obj1.pname+" , price="+obj1.price); 
 
            if(obj2.price<obj1.price && obj2.price<obj3.price)  
              System.out.println(" Product code= " +obj2.pcode+" , Product name="+obj2.pname+" , price="+obj2.price);
@@ -66,4 +66,24 @@ int price;
              
 }
 }
+
+/*INPUT
+
+Enter product1  pcode,pname,price: 
+ 23
+mat
+50
+Enter product 2  pcode,pname,price:
+ 24
+mango
+70
+Enter product 3  pcode,pname,price:
+ 25
+orange
+100
+
+OUTPUT
+
+ Product code= 23 , Productname=mat , price=50
+*/
 
