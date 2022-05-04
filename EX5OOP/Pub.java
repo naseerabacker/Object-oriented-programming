866f21626570dec502021f233124ede186de70ae
naseerabacker <95747989+naseerabacker@users.noreply.github.com>
2022-05-04 12:05:19 +0530

    Add files via upload

--- EX5OOP/Pub.java
+++ EX5OOP/Pub.java
@@ -11,7 +11,7 @@ import java.util.*;
 class Publisher
 {
     String pname;
-    Publisher(String n)//constructor
+    Publisher(String n)
     {
       pname=n;
     }
@@ -26,7 +26,7 @@ class Publisher
 class Book extends Publisher//subclass of Publisher
 {
    String title,author;
-   Book(String n,String t,String a)//constructor
+   Book(String n,String t,String a)
    {
      super(n);//invoking parent constructor
      title=t;
@@ -63,7 +63,7 @@ class Literature extends Book//subclass of book
 class Fiction extends Book//subclass of book
 {
      String genre;
-      Fiction(String n,String t,String a,String r)//constructor
+      Fiction(String n,String t,String a,String r)
       {
          super(n,t,a);//invoking parent constructor
          genre=r;
@@ -84,7 +84,7 @@ class Pub
       System.out.println("Enter Number of Books of literature: ");
      
       int n1=s.nextInt();
-      System.out.println("Enter Number of Books of Fictoin: ");
+      System.out.println("Enter Number of Books of Fiction: ");
      
       int n2=s.nextInt();
     
@@ -97,13 +97,13 @@ class Pub
          do
 	{
            System.out.println("\nEnter Literature Book details  "+(i+1));
-           System.out.println("Enter Publisher name :  ");
+           System.out.println("Enter publisher name :  ");
            String p=s.next();
-           System.out.println("Enter Book name:");
+           System.out.println("Enter book name:");
            String bk=s.next();
-           System.out.println("Enter Author name:");
+           System.out.println("Enter author name:");
            String au=s.next();
-           System.out.println("Enter Literature type:");
+           System.out.println("Enter literature type:");
            String lt=s.next();
            obj1[i]=new Literature(p,bk,au,lt);
             i++;
@@ -112,42 +112,47 @@ class Pub
          
        
      
-           do  {
-                System.out.println("\n\nEnter Fiction Book details  "+(j+1));
-           System.out.println("Enter Publisher name :  ");
+           do 
+	{
+           System.out.println("\n\nEnter fiction book details  "+(j+1));
+           System.out.println("Enter publisher name :  ");
            String p=s.next();
-           System.out.println("Enter Book name :  ");
+           System.out.println("Enter book name :  ");
            String bk=s.next();
-           System.out.println("Enter Author name :  ");
+           System.out.println("Enter author name :  ");
            String au=s.next();
            
-           System.out.println("Enter Fiction type :  ");
+           System.out.println("Enter fiction type :  ");
            String lt=s.next();
            obj2[j]=new Fiction(p,bk,au,lt);
-         j++;
+           j++;
 
-         }while(j<n2);
+         }
+	while(j<n2);
        
      	while(true)
           {
-     	System.out.println("\n\n1.LITERATURE\n2.FICTION\n3.EXIT\n\nEnter work type to show....\n");
+     	System.out.println("\n\n1.LITERATURE\n2.FICTION\n3.EXIT\n\nEnter work type to show \n");
              int ch2=s.nextInt();
-    switch(ch2){
-        case 1:
+          switch(ch2)
+	{
+           case 1:
            for(i=0;i<n1;i++)
-        {
-               System.out.println("\n____LITERATURE BOOK INFORMATION "+ (i+1)+"___\n");
+          {
+           System.out.println("\n____LITERATURE BOOK INFORMATION "+(i+1)+"___\n");
                obj1[i].display();
-             }
-       break;
-        case 2:
-                  for(j=0;j<n2;j++){
-               System.out.println("\n____FICTION BOOK INFORMATION "+(j+1)+"___\n");
-               obj2[j].display();    
+          }
+          break;
+           case 2:
+           for(j=0;j<n2;j++)
+	{
+           System.out.println("\n____FICTION BOOK INFORMATION "+(j+1)+"___\n");
+           obj2[j].display();    
          }
           break;
-    	case 3:System.exit(0);
-      	default:System.out.println("invalid");
+    	case 3:
+	 System.exit(0);
+      	default:System.out.println("INVALID");
        
       }
 
@@ -156,7 +161,93 @@ class Pub
 }
              
 
+/*
+Enter Number of Books of literature: 
+2
+Enter Number of Books of Fiction: 
+1
 
+Enter Literature Book details  1
+Enter publisher name :  
+bookis
+Enter book name:
+jungle
+Enter author name:
+john
+Enter literature type:
+english
+
+Enter Literature Book details  2
+Enter publisher name :  
+bookis
+Enter book name:
+baby
+Enter author name:
+meera
+Enter literature type:
+english
+
+
+Enter fiction book details  1
+Enter publisher name :  
+mack
+Enter book name :  
+moon
+Enter author name :  
+tom
+Enter fiction type :  
+english
+
+
+1.LITERATURE
+2.FICTION
+3.EXIT
+
+Enter work type to show 
+
+1
+
+____LITERATURE BOOK INFORMATION 1___
+
+Publisher name : bookis
+Book title : jungle
+Author : john
+Type : english
+
+____LITERATURE BOOK INFORMATION 2___
+
+Publisher name : bookis
+Book title : baby
+Author : meera
+Type : english
+
+
+1.LITERATURE
+2.FICTION
+3.EXIT
+
+Enter work type to show 
+
+2
+
+____FICTION BOOK INFORMATION 1___
+
+Publisher name : mack
+Book title : moon
+Author : tom
+Genre : english
+
+
+1.LITERATURE
+2.FICTION
+3.EXIT
+
+Enter work type to show 
+
+3
+ksb@ksb-ThinkCentre-Edge72:~/Desktop/naseeras2/java/EX5OOP$ 
+
+*/
 
 
 
