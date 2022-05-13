899879cbd3749ca2e5ac0f241128f8e21ee2fa31
naseerabacker <95747989+naseerabacker@users.noreply.github.com>
2022-05-13 15:47:30 +0530

    Delete third.java

--- EX6OOP/third.java
+++
@@ -1,7 +0,0 @@
-3
-4. Create a Graphics package that has classes and interfaces for
-figures Rectangle, Triangle, Square and Circle. Test the package by
-finding the area of these figures.
-5. Create an Arithmetic package that has classes and interfaces
-for the 4 basic arithmetic operations. Test the package by
-implementing all operations on two given numbers
